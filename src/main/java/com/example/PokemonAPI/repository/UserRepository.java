fbb4fae775a55b4a168a3bb48c62966edf153838
Jean Paul Opazo Robles <lzx10111@gmail.com>
2025-04-07 19:44:27 -0400

    Updated UserRepository Class

--- src/main/java/com/example/PokemonAPI/repository/UserRepository.java
+++ src/main/java/com/example/PokemonAPI/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.example.PokemonAPI.model.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsUsuarioByUsername(String username);
 
     @Query(value = "SELECT * FROM usuarios WHERE username = ?1", nativeQuery = true)
