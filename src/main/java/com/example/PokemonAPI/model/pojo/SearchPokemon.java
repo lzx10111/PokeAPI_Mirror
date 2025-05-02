ebdd2b9fa515aa9a4fdf76dc4cb0cc2358e8d9ab
Jean Paul Opazo Robles <lzx10111@gmail.com>
2025-05-01 23:38:22 -0400

    Updated SearchPokemon.java

--- src/main/java/com/example/PokemonAPI/model/pojo/SearchPokemon.java
+++ src/main/java/com/example/PokemonAPI/model/pojo/SearchPokemon.java
@@ -11,19 +11,29 @@ public class SearchPokemon {
     @Size(max = 10, message = "{idFilter.size}")
     private String idFilter;
 
-    @IntegerPositive(message = "{heightFilter.type}")
+    @IntegerPositive(allowEmptyValue = true, message = "{heightFilter.type}")
     @Size(max = 10, message = "{heightFilter.size}")
-    private String heightFilter;
+    private String heightFilterMax;
 
-    @IntegerPositive(message = "{weightFilter.type}")
+    @IntegerPositive(allowEmptyValue = true, message = "{heightFilter.type}")
     @Size(max = 10, message = "{heightFilter.size}")
-    private String weightFilter;
+    private String heightFilterMin;
 
-    public SearchPokemon(String nameFilter, String idFilter, String heightFilter, String weightFilter) {
+    @IntegerPositive(allowEmptyValue = true, message = "{weightFilter.type}")
+    @Size(max = 10, message = "{heightFilter.size}")
+    private String weightFilterMax;
+
+    @IntegerPositive(allowEmptyValue = true, message = "{weightFilter.type}")
+    @Size(max = 10, message = "{heightFilter.size}")
+    private String weightFilterMin;
+
+    public SearchPokemon(String nameFilter, String idFilter, String heightFilterMax, String heightFilterMin, String weightFilterMax, String weightFilterMin) {
         this.nameFilter = nameFilter;
         this.idFilter = idFilter;
-        this.heightFilter = heightFilter;
-        this.weightFilter = weightFilter;
+        this.heightFilterMax = heightFilterMax;
+        this.heightFilterMin = heightFilterMin;
+        this.weightFilterMax = weightFilterMax;
+        this.weightFilterMin = weightFilterMin;
     }
 
     public SearchPokemon() {
@@ -45,19 +55,47 @@ public class SearchPokemon {
         this.idFilter = idFilter;
     }
 
-    public String getHeightFilter() {
-        return heightFilter;
+    public String getHeightFilterMax() {
+        return heightFilterMax;
     }
 
-    public void setHeightFilter(String heightFilter) {
-        this.heightFilter = heightFilter;
+    public void setHeightFilterMax(String heightFilterMax) {
+        this.heightFilterMax = heightFilterMax;
     }
 
-    public String getWeightFilter() {
-        return weightFilter;
+    public String getHeightFilterMin() {
+        return heightFilterMin;
     }
 
-    public void setWeightFilter(String weightFilter) {
-        this.weightFilter = weightFilter;
+    public void setHeightFilterMin(String heightFilterMin) {
+        this.heightFilterMin = heightFilterMin;
+    }
+
+    public String getWeightFilterMax() {
+        return weightFilterMax;
+    }
+
+    public void setWeightFilterMax(String weightFilterMax) {
+        this.weightFilterMax = weightFilterMax;
+    }
+
+    public String getWeightFilterMin() {
+        return weightFilterMin;
+    }
+
+    public void setWeightFilterMin(String weightFilterMin) {
+        this.weightFilterMin = weightFilterMin;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchPokemon{" +
+                "nameFilter='" + nameFilter + '\'' +
+                ", idFilter='" + idFilter + '\'' +
+                ", heightFilterMax='" + heightFilterMax + '\'' +
+                ", heightFilterMin='" + heightFilterMin + '\'' +
+                ", weightFilterMax='" + weightFilterMax + '\'' +
+                ", weightFilterMin='" + weightFilterMin + '\'' +
+                '}';
     }
 }
